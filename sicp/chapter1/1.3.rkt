20b8f373f93effc18968e4650ab26ea35cca4444
berdnikovdmitry <berdikovbernikov1@gmail.com>
2021-08-08 22:30:33 +0300

    refactor 1.3

--- sicp/chapter1/1.3.rkt
+++ sicp/chapter1/1.3.rkt
@@ -5,8 +5,8 @@
 (define (square x) (* x x))
 (define (sum-of-squares x y) (+ (square x) (square y)))
 (define (sum-of-squares-of-two-largest-out-of-three x y z) (
-  cond ((and (<= x y) (<= x z) (sum-of-squares y z)))
-       ((and (<= y z) (<= y x) (sum-of-squares x z)))
+  cond ((and (< x y) (< x z) (sum-of-squares y z)))
+       ((and (< y z) (< y x) (sum-of-squares x z)))
        (else (sum-of-squares x y))
 ))
 
