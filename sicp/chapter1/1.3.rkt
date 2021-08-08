fae58e2f3ce4f39c55163d1634ebc38cdfbe29ed
berdnikovdmitry <berdikovbernikov1@gmail.com>
2021-08-08 22:23:40 +0300

    add 1.3.rkt

--- sicp/chapter1/1.3.rkt
+++ sicp/chapter1/1.3.rkt
@@ -5,18 +5,15 @@
 (define (square x) (* x x))
 (define (sum-of-squares x y) (+ (square x) (square y)))
 (define (sum-of-squares-of-two-largest-out-of-three x y z) (
-  cond ((and (<= x y z) (sum-of-squares y z)))
-       ((and (<= y x z) (sum-of-squares x z)))
+  cond ((and (<= x y) (<= x z) (sum-of-squares y z)))
+       ((and (<= y z) (<= y x) (sum-of-squares x z)))
        (else (sum-of-squares x y))
 ))
 
 (check-equal? (sum-of-squares-of-two-largest-out-of-three 1 2 3) 13)
 (check-equal? (sum-of-squares-of-two-largest-out-of-three 4 3 2) 25)
-
-; todo pass success
 (check-equal? (sum-of-squares-of-two-largest-out-of-three 5 3 4) 41)
-
-; (check-equal? (sum-of-squares-of-two-largest-out-of-three 3 3 2) 18)
-; (check-equal? (sum-of-squares-of-two-largest-out-of-three 4 3 4) 32)
-; (check-equal? (sum-of-squares-of-two-largest-out-of-three 5 2 10) 125)
-; (check-equal? (sum-of-squares-of-two-largest-out-of-three 5 5 5) 50)
+(check-equal? (sum-of-squares-of-two-largest-out-of-three 3 4 4) 32)
+(check-equal? (sum-of-squares-of-two-largest-out-of-three 4 3 4) 32)
+(check-equal? (sum-of-squares-of-two-largest-out-of-three 4 4 3) 32)
+(check-equal? (sum-of-squares-of-two-largest-out-of-three 3 3 3) 18)
