455816b00edde720c63c2bba3881281b1bf9c7ab
berdnikovdmitry <berdikovbernikov1@gmail.com>
2021-08-13 22:30:07 +0300

    start 1.6

--- sicp/chapter1/1.6.rkt
+++ sicp/chapter1/1.6.rkt
@@ -8,4 +8,25 @@
   (cond (predicate then-clause)
         (else else-clause)))
 
-(check-equal? (new-if (> 6 5) 10 20) 20)
+; (check-equal? (new-if (> 6 5) 10 20) 20)
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x) (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(check-equal? (sqrt 24) 3)
